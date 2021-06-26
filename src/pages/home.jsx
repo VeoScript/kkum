8a7410fd6aef8b96e806e0647716fd58beca4086
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-26 23:13:16 +0800

    feat: update prisma schema

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,20 @@
+import Head from 'next/head'
 import { getSession } from 'next-auth/client'
 
 export default function Home({ user }) {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome Kkum {user.email}</p>
-    </div>
+    <>
+      <Head>
+        <title>Kkum | Home</title>
+      </Head>
+      <div className="flex flex-row justify-center items-center w-full h-screen">
+        <div className="flex flex-col items-center w-full">
+          <h1>Home</h1>
+          <img className="w-20 h-20 rounded-full object-cover" src={user.image} alt="avatar" />
+          <p>Welcome to Kkum {user.email}</p>
+        </div>
+      </div>
+    </>
   )
 }
 
