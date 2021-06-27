e908a1eed970895b66f0b267b7b70ba0badd1e89
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-27 18:47:41 +0800

    style: change colors, design and layouts

--- src/pages/guard/verify-email.jsx
+++ src/pages/guard/verify-email.jsx
@@ -7,22 +7,22 @@ export default function VerifyEmail() {
       <Head>
         <title>Kkum | Email Verification</title>
       </Head>
-      <div className="flex flex-col items-center justify-center w-full h-screen bg-[#F4EBE3]">
+      <div className="flex flex-col items-center justify-center w-full h-screen bg-[#131E29]">
         <div className="flex flex-col items-center w-full">
-          <h1 className="font-black text-5xl text-[#A06647]">Kkum</h1>
-          <h3 className="font-light text-lg text[#333]">Open the portal of your imagination.</h3>
+          <h1 className="font-black text-5xl text-[#CCB57F]">Kkum</h1>
+          <h3 className="font-light text-base text-[#fff]">Open the portal of your imagination.</h3>
         </div>
         <div className="flex flex-col items-center justify-center w-full mt-10">
           <div className="flex flex-col items-center text-center w-full max-w-md space-y-5">
-            <h1 className="font-normal text-3xl text-[#222]">Verify your email address</h1>
-            <p className="font-normal text-lg text-[#222]">
-              Please check your email to confirm that you want to use this as your <span className="font-black text-xl text-[#A06647]">Kkum</span> account
+            <h1 className="font-normal text-3xl text-[#fff]">Verify your email address</h1>
+            <p className="font-normal text-lg text-[#fff]">
+              Please check your email to confirm that you want to use this as your <span className="font-black text-xl text-[#CCB57F]">Kkum</span> account
               email address. Once it's done you will be able to start your odyssey.
             </p>
           </div>
           <div className="flex flex-col items-center w-full">
             <Link href="/">
-              <a className="px-5 py-3 mt-10 w-full max-w-sm rounded-lg shadow-2xl transition ease-in-out duration-300 transform hover:scale-95 text-center text-[#fff] bg-[#C6987E]">
+              <a className="px-5 py-3 mt-10 w-full max-w-sm rounded-lg shadow-2xl transition ease-in-out duration-300 transform hover:scale-95 text-center text-[#fff] bg-[#1A2D38]">
                 Return
               </a>
             </Link>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,31 @@
 import Head from 'next/head'
 import { getSession } from 'next-auth/client'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home({ user }) {
+const prisma = new PrismaClient()
+
+export default function Home({ getUser }) {
   return (
     <>
       <Head>
         <title>Kkum | Home</title>
       </Head>
-      <div className="flex flex-row justify-center items-center w-full h-screen">
-        <div className="flex flex-col items-center w-full">
-          <h1>Home</h1>
-          <img className="w-20 h-20 rounded-full object-cover" src={user.image} alt="avatar" />
-          <p>Welcome to Kkum {user.email}</p>
+      <div className="flex flex-row justify-between w-full h-screen text-[#fff] bg-[#131E29]">
+        <div className="flex flex-col w-full max-w-xs py-10 bg-[#1A2D38]">
+          <div className="flex flex-col items-center w-full space-y-3">
+            <img
+              src={getUser.image}
+              alt="avatar"
+              className="w-20 h-20 rounded-full object-cover"
+            />
+            <div className="flex flex-col items-center">
+              <h1 className="font-bold text-xl">{ getUser.name }</h1>
+              <h1 className="font-normal text-sm">{ getUser.bio }</h1>
+            </div>
+          </div>
+          <div className="flex flex-col items-center w-full">
+
+          </div>
         </div>
       </div>
     </>
@@ -25,10 +39,20 @@ export async function getServerSideProps(ctx) {
     ctx.res.end()
     return {}
   }
-
+  const getUser = await prisma.user.findFirst({
+    where: {
+      email: session.user.email
+    },
+    select: {
+      image: true,
+      name: true,
+      bio: true
+    }
+  })
   return {
     props: {
-      user: session.user,
+      // user: session.user,
+      getUser
     },
   }
 }
