b686bc93896d29f6c26cd8363a953bddac4446b8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-28 11:57:12 +0800

    feat: add dialog menu in profile & max width in default layout

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -2,10 +2,12 @@ import NavigationBar from '~/components/NavigationBar'
 
 export default function Layout({ children, getUser }) {
   return (
-    <div className="flex flex-col w-full h-screen text-kkum-white bg-kkum-dark">
-      <NavigationBar getUser={getUser} />
-      <div className="flex w-full h-full px-32 py-5">
-        { children }
+    <div className="flex flex-col items-center w-full max-w-full h-screen text-kkum-white bg-kkum-dark">
+      <div className="flex flex-col w-full max-w-screen-2xl h-full">
+        <NavigationBar getUser={getUser} />
+        <div className="flex w-full max-w-6xl h-full px-32 py-5">
+          { children }
+        </div>
       </div>
     </div>
   )
--- src/static/navigations.js
+++ src/static/navigations.js
@@ -7,11 +7,24 @@ export const navigations = [
   {
     name: 'Notifications',
     icon: <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>,
-    href: '/home'
+    href: '/notifications'
   },
   {
     name: 'Chats',
     icon: <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"></path></svg>,
-    href: '/home'
+    href: '/chats'
+  }
+]
+
+export const profile_menus = [
+  {
+    name: 'Dreams',
+    icon: <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>,
+    href: '/dreams'
+  },
+  {
+    name: 'Settings',
+    icon: <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>,
+    href: '/settings'
   }
 ]
