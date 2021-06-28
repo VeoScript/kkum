4b63c12b1d7701f6760f78ac4886b4e2e04f1331
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-28 11:05:06 +0800

    feat: add navigation links and navigation bar component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Layout from '~/layouts/default'
 import { getSession } from 'next-auth/client'
 import { PrismaClient } from '@prisma/client'
 
@@ -10,29 +11,11 @@ export default function Home({ getUser }) {
       <Head>
         <title>Kkum | Home</title>
       </Head>
-      <div className="flex flex-row justify-between w-full h-screen text-kkum-white bg-kkum-dark">
-        <div className="sidebar flex flex-col w-full max-w-xs py-10 bg-[#1A2D38]">
-          <div className="flex flex-col items-center w-full">
-            Display Web Title & and Navigation Menus
-          </div>
+      <Layout getUser={getUser}>
+        <div>
+          This is Final Content
         </div>
-        <div className="flex flex-col w-full h-full">
-          <div className="topbar flex flex-row justify-between items-center w-full px-5 py-2 space-x-5 bg-kkum-mid">
-            <div className="searchbox flex flex-row items-center w-full max-w-full bg-light-panther rounded-full">
-              <input
-                className="w-full px-5 py-3 bg-light-panther rounded-full focus:outline-none"
-                placeholder="Search"
-              />
-              <svg className="w-6 h-6 text-gray-400 mr-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-              </svg>
-            </div>
-            <div className="links flex justify-end w-full">
-              Display Links and Profile
-            </div>
-          </div>
-        </div>
-      </div>
+      </Layout>
     </>
   )
 }
@@ -56,7 +39,6 @@ export async function getServerSideProps(ctx) {
   })
   return {
     props: {
-      // user: session.user,
       getUser
     },
   }
---
+++ src/static/navigations.js
@@ -0,0 +1,17 @@
+export const navigations = [
+  {
+    name: 'Home',
+    icon: <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"></path></svg>,
+    href: '/home'
+  },
+  {
+    name: 'Notifications',
+    icon: <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>,
+    href: '/home'
+  },
+  {
+    name: 'Chats',
+    icon: <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"></path></svg>,
+    href: '/home'
+  }
+]
