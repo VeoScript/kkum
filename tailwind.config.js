f73f6e61f91270e32331742049c6736149e2e146
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-28 09:52:54 +0800

    style: add custom default color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -6,7 +8,17 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'kkum-white': '#F4F9FF',
+        'kkum-dark': '#131E29',
+        'kkum-mid': '#1A2D38',
+        'kkum-khaki': '#CCB57F',
+      },
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.mono]
+      }
+    },
   },
   variants: {
     extend: {},
