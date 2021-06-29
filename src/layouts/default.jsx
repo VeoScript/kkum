fcda58ad1d71d192e208912fb3006d7647be1625
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-29 10:57:28 +0800

    chore: update home-content component (#20)

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -2,7 +2,7 @@ import NavigationBar from '~/components/NavigationBar'
 
 export default function Layout({ children, getUser }) {
   return (
-    <div className="flex flex-col items-center w-full max-w-full h-screen text-kkum-white bg-kkum-dark">
+    <div className="flex flex-col items-center w-full max-w-full h-screen overflow-hidden text-kkum-white bg-kkum-dark">
       <div className="flex flex-col w-full max-w-screen-2xl h-full">
         <NavigationBar getUser={getUser} />
         <div className="flex w-full max-w-6xl h-full px-32 py-5">
