19b936634eef94e69e724549e76f0ddc7a140798
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-29 13:25:08 +0800

    feat: add nextjs-progressbar (#21)

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -32,7 +32,7 @@ export default function HomeContent({ getUser }) {
           </div>
         </div>
         <div className="flex flex-row items-center text-sm text-gray-400">
-          <Link href="/home">
+          <Link href="/profile">
             <a className="flex flex-row items-center justify-center w-full px-3 py-3 space-x-2 bg-kkum-dark bg-opacity-50 hover:bg-opacity-30">
               <svg className="w-4 h-4 text-kkum-khaki" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,11 @@
 import '~/styles/tailwind.css'
+import NextNProgress from '~/lib/NextProgressBar'
 import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <NextNProgress />
       <Component {...pageProps} />
     </Provider>
   )
